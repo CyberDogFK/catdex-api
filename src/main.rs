aab0bcb0fc378534f8dd5d46047d0d6476e1f64d
Anton Pavliuk <streetreadger@gmail.com>
2024-02-12 21:07:51 +0200

    Add simple example of http server
    There we directly send static page with static images

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_files::{Files, NamedFile};
+use actix_web::{web, App, HttpServer, Result};
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world")
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
 }
 
 #[actix_web::main]
@@ -9,7 +10,15 @@ async fn main() -> std::io::Result<()> {
     println!("Listening on port 8080");
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(hello))
+            .service(
+                Files::new("/static", "static")
+                    .show_files_listing(),
+            )
+            .service(
+                Files::new("/image", "image")
+                    .show_files_listing(),
+            )
+            .route("/", web::get().to(index))
     })
         .bind("127.0.0.1:8080")?
         .run()
