c75dfbe3f7dc26abda54c2167d5e7d0634c006cf
Anton Pavliuk <streetreadger@gmail.com>
2024-02-14 09:57:21 +0200

    Implement add cat endpoint

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use actix_web::{error, web, App, Error, HttpResponse, HttpServer, Result};
 use diesel::r2d2::ConnectionManager;
 use diesel::{PgConnection, QueryDsl, RunQueryDsl};
 use std::env;
+use std::time::Duration;
+use std::collections::HashMap;
 
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
@@ -24,11 +26,46 @@ async fn cats_endpoint(pool: web::Data<DbPool>) -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().json(cats_data))
 }
 
+async fn add_cat_endpoint(
+    pool: web::Data<DbPool>,
+    mut parts: awmp::Parts,
+) -> Result<HttpResponse, Error> {
+    let file_path = parts
+        .files
+        .take("image")
+        .pop()
+        .and_then(|f| f.persist_in("./image").ok())
+        .unwrap_or_default();
+
+    let text_fields: HashMap<_, _> =
+        parts.texts.as_pairs().into_iter().collect();
+
+    let mut connection = pool.get()
+        .expect("Can't get db connection from pool");
+
+    let new_cat = NewCat {
+        name: text_fields.get("name")
+            .unwrap().to_string(),
+        image_path: file_path.to_string_lossy().to_string()
+    };
+
+    web::block(move ||
+        diesel::insert_into(cats)
+            .values(&new_cat)
+            .execute(&mut connection))
+        .await
+        .map_err(error::ErrorInternalServerError)?
+        .map_err(error::ErrorInternalServerError)?;
+
+    Ok(HttpResponse::Created().finish())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(&database_url);
     let pool = r2d2::Pool::builder()
+        .connection_timeout(Duration::from_secs(5))
         .build(manager)
         .expect("Failed to create DB connection pool.");
 
@@ -37,9 +74,16 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            .app_data(
+                awmp::PartsConfig::default().with_temp_dir("./tmp")
+            )
             .service(Files::new("/static", "static").show_files_listing())
             .service(Files::new("/image", "image").show_files_listing())
-            .service(web::scope("/api").route("/cats", web::get().to(cats_endpoint)))
+            .service(
+                web::scope("/api")
+                    .route("/cats", web::get().to(cats_endpoint))
+                    .route("/add_cat", web::post().to(add_cat_endpoint))
+            )
             .route("/", web::get().to(index))
     })
     .bind("127.0.0.1:8080")?
--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,18 @@
-use diesel::Queryable;
+use diesel::{Queryable, Insertable};
 use serde::Serialize;
+use crate::schema::cats;
 
 #[derive(Queryable, Serialize)]
 pub struct Cat {
     pub id: i32,
+    pub name: String,
+    pub image_path: String,
+}
+
+#[derive(Insertable, Serialize)]
+#[diesel(table_name = cats)]
+pub struct NewCat {
+    // id will be added by the database
     pub name: String,
     pub image_path: String,
 }
